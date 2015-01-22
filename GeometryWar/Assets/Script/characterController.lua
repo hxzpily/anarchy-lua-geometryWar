c0c4ea583c8f51aee9b366e1ae6a8f77ff7f8e24
Blackhart <manceau.thomas19@gmail.com>
2015-01-23 00:44:22 +0100

    Life of the player added

--- GeometryWar/Assets/Script/characterController.lua
+++ GeometryWar/Assets/Script/characterController.lua
@@ -1,29 +1,37 @@
 -- new script file
+
 function OnAfterSceneLoaded(self)
 
 self.movementSpeed = 100.0f;
 self.orientationSpeed = 200.0f;
 self.defaultFireRate = 0.2f;
-self.fireRate = self.defaultFireRate;
 self.bulletDefaultPos = Game:GetEntity("bulletDefaultPos");
+self.defaultLife = 5.0f;
 
 self.minEdgeX = Game:GetEntity("collBottom"):GetPosition().x;
 self.maxEdgeX = Game:GetEntity("collTop"):GetPosition().x;
 self.minEdgeY = Game:GetEntity("collLeft"):GetPosition().y;
 self.maxEdgeY = Game:GetEntity("collRight"):GetPosition().y;
 
+self.currentLife = self.defaultLife;
+self.fireRate = self.defaultFireRate;
+
 end
+
 
 function OnThink(self)
 
 moveCharacter(self);
 orientCharacter(self);
 fire(self);
+Debug:PrintLine(tostring(self.currentLife));
 
 end
 
+
 function moveCharacter(self)
 
+-- Move the player to his dir
 if (Input:IsKeyPressed(Vision.KEY_Z)) then
   self:IncPosition(self:GetObjDir_Right() * self.movementSpeed * Timer:GetTimeDiff());
 end
@@ -31,12 +39,14 @@ if (Input:IsKeyPressed(Vision.KEY_S)) then
   self:IncPosition(-self:GetObjDir_Right() * self.movementSpeed * Timer:GetTimeDiff());
 end
 
+-- check that the player is not out of bounds (X axis)
 if (self:GetPosition().x > self.maxEdgeX) then
   self:SetPosition(self.maxEdgeX, self:GetPosition().y, self:GetPosition().z);
 elseif (self:GetPosition().x < self.minEdgeX) then
   self:SetPosition(self.minEdgeX, self:GetPosition().y, self:GetPosition().z);
 end
 
+-- check that the player is not out of bounds (Y axis)
 if (self:GetPosition().y > self.maxEdgeY) then
   self:SetPosition(self:GetPosition().x, self.maxEdgeY, self:GetPosition().z);
 elseif (self:GetPosition().y < self.minEdgeY) then
@@ -44,6 +54,7 @@ elseif (self:GetPosition().y < self.minEdgeY) then
 end
 
 end
+
 
 function orientCharacter(self)
 
@@ -55,6 +66,7 @@ if (Input:IsKeyPressed(Vision.KEY_Q)) then
 end
 
 end
+
 
 function fire(self)
 
--- GeometryWar/Assets/Script/playerTriggerHit.lua
+++ GeometryWar/Assets/Script/playerTriggerHit.lua
@@ -4,6 +4,7 @@ function OnObjectEnter(self, object)
 --Debug:PrintLine("Hit");
 if (object.ennemy == true) then
   Debug:PrintLine("je suis un ennemy");
+  self:GetParent().currentLife = self:GetParent().currentLife - 1.0f;
   object:Remove();
 end
 
