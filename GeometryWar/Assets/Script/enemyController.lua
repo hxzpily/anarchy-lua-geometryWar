75f3fd9c6de70dc15dfb85f6a860aaa5ded29ec7
Blackhart <manceau.thomas19@gmail.com>
2015-01-23 02:14:31 +0100

    Generation of enemy improved

--- GeometryWar/Assets/Script/enemyController.lua
+++ GeometryWar/Assets/Script/enemyController.lua
@@ -1,43 +1,29 @@
 -- new script file
 
 function OnAfterSceneLoaded(self)
-
-initPrefab(self);
-
+  initPrefab(self);
 end
 
 
-
 function initPrefab(self)
-
-self.init = true;
-self.ennemy = true;
-self.defaultGain = 5.0f;
-self.speed = math.random(30.0f, 50.0f);
-
-self.target = Game:GetEntity("Character");
-
+  self.init = true;
+  self.ennemy = true;
+  self.defaultGain = 5.0f;
+  self.speed = math.random(30.0f, 50.0f);
+  self.target = Game:GetEntity("Character");
 end
 
 
 function OnThink(self)
-
-if (self.init == nil) then
-
-  initPrefab(self);
-  
+  if (self.init == nil) then
+    initPrefab(self);
+  end
+  move(self);
 end
-
-move(self);
-
-end
-
 
 
 function move(self)
-
-local lDir = self.target:GetPosition() - self:GetPosition();
-lDir:normalize();
-self:SetPosition(self:GetPosition() + lDir * self.speed * Timer:GetTimeDiff());
-
+  local lDir = self.target:GetPosition() - self:GetPosition();
+  lDir:normalize();
+  self:SetPosition(self:GetPosition() + lDir * self.speed * Timer:GetTimeDiff());
 end
--- GeometryWar/Assets/Script/enemyGenerator.lua
+++ GeometryWar/Assets/Script/enemyGenerator.lua
@@ -1,52 +1,38 @@
 -- new script file
 
 function OnAfterSceneLoaded(self)
-
-self.defaultGenRate = 5.0f;
-self.defaultNbSpawnPerRate = 1.0f;
-self.defaultSpawnPos = { Game:GetEntity("EnemySpawn1"):GetPosition(),
-                         Game:GetEntity("EnemySpawn2"):GetPosition(),
-                         Game:GetEntity("EnemySpawn3"):GetPosition(),
-                         Game:GetEntity("EnemySpawn4"):GetPosition(),
-                         Game:GetEntity("EnemySpawn5"):GetPosition(),
-                         Game:GetEntity("EnemySpawn6"):GetPosition(),
-                         Game:GetEntity("EnemySpawn7"):GetPosition(),
-                         Game:GetEntity("EnemySpawn8"):GetPosition(),
-                         Game:GetEntity("EnemySpawn9"):GetPosition(),
-                         Game:GetEntity("EnemySpawn10"):GetPosition() };
-
-
-self.currentGenRate = self.defaultGenRate;
-
+  self.defaultGenRate = 5.0f;
+  self.defaultNbSpawnPerRate = 1.0f;
+  self.defaultSpawnPos = { };
+  getDefaultSpawn(self);
+  self.currentGenRate = self.defaultGenRate;
 end
 
+
+function getDefaultSpawn(self)
+  local i = 0;
+  while i < 10 do
+    local lName = "EnemySpawn" .. tostring(i+1);
+    Debug:PrintLine(lName);
+    self.defaultSpawnPos[i] = Game:GetEntity(lName):GetPosition();
+    i = i + 1;
+  end
+end
 
 
 function OnThink(self)
-
-generateEnemy(self);
-
+  generateEnemy(self);
 end
-
 
 
 function generateEnemy(self)
-
-self.currentGenRate = self.currentGenRate - Timer:GetTimeDiff();
-
-if (self.currentGenRate <= 0.0f) then
-  
-  local i = 0.0f;
-
-  while i < self.defaultNbSpawnPerRate do
-  
-    Game:InstantiatePrefab(self.defaultSpawnPos[math.random(0, 10)], "Prefabs/Ennemy.vprefab", nil);
-    i = i + 1;
-    
+  self.currentGenRate = self.currentGenRate - Timer:GetTimeDiff();
+  if (self.currentGenRate <= 0.0f) then
+    local i = 0.0f;
+    while i < self.defaultNbSpawnPerRate do
+      Game:InstantiatePrefab(self.defaultSpawnPos[math.random(0, 10)], "Prefabs/Ennemy.vprefab", nil);
+      i = i + 1.0f;
+    end
+    self.currentGenRate = self.defaultGenRate;
   end
-  
-  self.currentGenRate = self.defaultGenRate;
-
-end
-
 end
